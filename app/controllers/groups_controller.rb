c61ae96045d17539398d3ccc1de2d660d43fba62
eiryu04 <legendwing0403@gmail.com>
2020-01-14 00:55:53 +0900

    メッセージ送信機能実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,14 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
-
+  
   def index
   end
-  
+
   def new
     @group = Group.new
     @group.users << current_user
   end
+
   def create
     @group = Group.new(group_params)
       if @group.save
@@ -16,22 +17,26 @@ class GroupsController < ApplicationController
         render :new
       end
   end
+  
   def edit
   end
+  
   def update
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
-
   end
+
   private
+  
   def group_params
-    params.require(:group).permit(:name, user_ids:[])
+    params.require(:group).permit(:name, user_ids: [])
   end
 
   def set_group
     @group = Group.find(params[:id])
   end
+  
 end
