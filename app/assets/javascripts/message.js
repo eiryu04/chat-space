367164bbaa305cd5c9a81c9ce6f965e8037179db
eiryu04 <legendwing0403@gmail.com>
2020-01-31 20:23:43 +0900

    メッセージ画面の自動更新機能追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,56 @@
 $(function(){
-  function buildHTML(message){
-    if( message.image ){
-      var html = 
-      `<div class="messages__message">
-        <div class = "messages__message__info">
-          <div class = "messages__message__info__talker">
-            ${message.user_name}
-          </div>
-          <div class="messages__message__info__date">
-            ${message.created_at}
-          </div>
-      </div>
-        <div class="messages__lower-message">
-          <p class="messages__message__text">
-          ${message.content}
-          </p>
-        </div>
-        <img src=${message.image}>
-      </div>`
-    return html;
-    } else {
-      var html = 
-      `<div class="messages__message">
-        <div class = "messages__message__info">
-          <div class = "messages__message__info__talker">
-            ${message.user_name}
-          </div>
-          <div class="messages__message__info__date">
-            ${message.created_at}
-          </div>
-      </div>
-        <div class="messages__lower-message">
-          <p class="messages__message__text">
-          ${message.content}
-          </p>
-        </div>
-      </div>`
-      return html;
-      };
-    }
+  var buildHTML = function(message){
+    if (message.content && message.image) {
+      var html = `<div class="messages__message" data-message-id=` + message.id + `>` +
+          `<div class = "messages__message__info">` +
+            `<div class = "messages__message__info__talker">` +
+              message.user_name +
+            `</div>` +
+            `<div class="messages__message__info__date">` +
+              message.created_at +
+            `</div>` +
+        `</div>` +
+        `<div class="messages__lower-message">` +
+          `<p class="messages__message__text">` +
+            message.content +
+          `</p>` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    } else if (message.content) {
+      var html = `<div class="messages__message" data-message-id=` + message.id + `>` +
+        `<div class = "messages__message__info">` +
+         `<div class = "messages__message__info__talker">` +
+            message.user_name +
+          `</div>` +
+          `<div class="messages__message__info__date">` +
+            message.created_at +
+          `</div>` +
+      `</div>` +
+      `<div class="messages__lower-message">` +
+          `<p class="messages__message__text">` +
+            message.content +
+          `</p>` +
+        `</div>` +
+      `</div>`
+    } else if (message.image) {
+      var html = `<div class="messages__message" data-message-id=` + message.id + `>` +
+        `<div class = "messages__message__info">` +
+        `<div class = "messages__message__info__talker">` +
+            message.user_name +
+          `</div>` +
+          `<div class="messages__message__info__date">` +
+            message.created_at +
+          `</div>` +
+      `</div>` +
+        `<div class="messages__lower-message">` +
+          `<img src ="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    };
+     return html;
+  };
+
 $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -61,5 +73,31 @@ $('#new_message').on('submit', function(e){
      .fail(function(){
        alert("メッセージ送信に失敗しました");
      })
-})
+});
+
+  var reloadMessages = function() {
+    last_message_id = $('.messages__message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function(){
+      console.log('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
